12a609f1ec1c2372cd060ed49a5ed6ee2821f25b
saulprl <phasecross@hotmail.com>
2024-06-08 20:23:10 -0700

    Basic HTML structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <h1>Saul&apos;s ToDo-List</h1>
+      <form>
+        <input type="text" placeholder="Write a ToDo!" />
+        <button>Submit</button>
+      </form>
+      <h2>My ToDos</h2>
+      <ul className="list-inside list-disc">
+        <li>Attend the React course</li>
+        <li>Build a ToDo-List</li>
+        <li>???</li>
+        <li>Profit!</li>
+      </ul>
+    </main>
+  );
 }
 
-export default App
+export default App;
