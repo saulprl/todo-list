03e4155868950e32b9c9b269ccf706717af66800
saulprl <phasecross@hotmail.com>
2024-06-09 18:01:22 -0700

    Improve interactivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { ChangeEventHandler, FormEventHandler, useState } from "react";
 
 const DUMMY_TODOS = [
-  { id: 1, title: "Attend the React course" },
-  { id: 2, title: "Build a ToDo-List" },
-  { id: 3, title: "???" },
+  { id: 1, title: "Attend the React course", completed: true },
+  { id: 2, title: "Build a ToDo-List", completed: false },
+  { id: 3, title: "???", completed: false },
 ];
 
 function App() {
@@ -13,10 +13,20 @@ function App() {
   const handleSubmit: FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
 
+    if (!inputValue.trim()) {
+      setInputValue("");
+
+      return;
+    }
+
     setTodos((prevTodos) => {
       return [
         ...prevTodos,
-        { id: Math.random() * 1000 + prevTodos.length + 1, title: inputValue },
+        {
+          id: Math.floor(Math.random() * 1000 + prevTodos.length + 1),
+          title: inputValue,
+          completed: false,
+        },
       ];
     });
 
@@ -25,6 +35,21 @@ function App() {
 
   const handleInputChange: ChangeEventHandler<HTMLInputElement> = (event) => {
     setInputValue(event.target.value);
+  };
+
+  const handleCheckedChange = (todoId: number) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === todoId) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+
+        return todo;
+      });
+    });
   };
 
   const deleteTodo = (todoId: number) => {
@@ -52,6 +77,7 @@ function App() {
               placeholder="Write a ToDo!"
               value={inputValue}
               onChange={handleInputChange}
+              required
               className="bg-violet-300 rounded-md text-black placeholder-violet-700/50 p-2 flex-grow"
             />
             <button className="p-2 bg-[#7145d6] text-white rounded-md hover:bg-violet-700 transition-colors duration-300">
@@ -64,10 +90,34 @@ function App() {
             {todos.map((todo) => (
               <li
                 key={todo.id}
-                onClick={deleteTodo.bind(null, todo.id)}
-                className="w-full p-2 rounded-md bg-violet-400"
+                onClick={() => handleCheckedChange(todo.id)}
+                className="flex items-center justify-between w-full p-2 rounded-md bg-violet-400"
               >
-                {todo.title}
+                <div className="flex items-center gap-2">
+                  <input
+                    id={`todo-${todo.id}`}
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={handleCheckedChange.bind(null, todo.id)}
+                    className="peer accent-[#7145d6] size-4 rounded-sm border-2 border-[#7145d6] bg-violet-200 checked:bg-[#7145d6]"
+                  />
+                  <label
+                    htmlFor={`todo-${todo.id}`}
+                    className="peer-checked:line-through peer-checked:text-gray-500 transition-colors duration-300"
+                  >
+                    {todo.title}
+                  </label>
+                </div>
+                <button
+                  onClick={(event) => {
+                    event.stopPropagation();
+
+                    deleteTodo(todo.id);
+                  }}
+                  className="px-2 py-1 rounded bg-[#e91e63] text-white"
+                >
+                  Delete
+                </button>
               </li>
             ))}
           </ul>
