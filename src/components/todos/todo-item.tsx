39a6501f7010645f354ef88131b9842b2aa99900
saulprl <phasecross@hotmail.com>
2024-06-09 19:05:06 -0700

    Abstract todo item

---
+++ src/components/todos/todo-item.tsx
@@ -0,0 +1,42 @@
+import { FC } from "react";
+
+interface Props {
+  todo: { id: number; title: string; completed: boolean };
+  onChecked: (todoId: number) => void;
+  onDeleteTodo: (todoId: number) => void;
+}
+
+export const TodoItem: FC<Props> = ({ todo, onChecked, onDeleteTodo }) => {
+  return (
+    <li
+      onClick={() => onChecked(todo.id)}
+      className="flex items-center justify-between w-full p-2 rounded-md bg-violet-400"
+    >
+      <div className="flex items-center gap-2">
+        <input
+          id={`todo-${todo.id}`}
+          type="checkbox"
+          checked={todo.completed}
+          onChange={onChecked.bind(null, todo.id)}
+          className="peer accent-[#7145d6] size-4 rounded-sm border-2 border-[#7145d6] bg-violet-200 checked:bg-[#7145d6]"
+        />
+        <label
+          htmlFor={`todo-${todo.id}`}
+          className="peer-checked:line-through peer-checked:text-gray-500 transition-colors duration-300"
+        >
+          {todo.title}
+        </label>
+      </div>
+      <button
+        onClick={(event) => {
+          event.stopPropagation();
+
+          onDeleteTodo(todo.id);
+        }}
+        className="px-2 py-1 rounded bg-[#e91e63] text-white"
+      >
+        Delete
+      </button>
+    </li>
+  );
+};
--- src/components/todos/todo-list.tsx
+++ src/components/todos/todo-list.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { TodoItem } from "./todo-item";
 
 interface Props {
   todos: { id: number; title: string; completed: boolean }[];
@@ -15,37 +16,12 @@ export const TodoList: FC<Props> = ({
     <div className="flex w-full h-full flex-grow flex-col rounded-xl p-2 bg-violet-200">
       <ul className="flex flex-col gap-2">
         {todos.map((todo) => (
-          <li
+          <TodoItem
             key={todo.id}
-            onClick={() => onCheckedChange(todo.id)}
-            className="flex items-center justify-between w-full p-2 rounded-md bg-violet-400"
-          >
-            <div className="flex items-center gap-2">
-              <input
-                id={`todo-${todo.id}`}
-                type="checkbox"
-                checked={todo.completed}
-                onChange={onCheckedChange.bind(null, todo.id)}
-                className="peer accent-[#7145d6] size-4 rounded-sm border-2 border-[#7145d6] bg-violet-200 checked:bg-[#7145d6]"
-              />
-              <label
-                htmlFor={`todo-${todo.id}`}
-                className="peer-checked:line-through peer-checked:text-gray-500 transition-colors duration-300"
-              >
-                {todo.title}
-              </label>
-            </div>
-            <button
-              onClick={(event) => {
-                event.stopPropagation();
-
-                onDeleteTodo(todo.id);
-              }}
-              className="px-2 py-1 rounded bg-[#e91e63] text-white"
-            >
-              Delete
-            </button>
-          </li>
+            todo={todo}
+            onChecked={onCheckedChange}
+            onDeleteTodo={onDeleteTodo}
+          />
         ))}
       </ul>
     </div>
