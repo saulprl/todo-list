4c3248414af3b174b048e66aa0ab1cdc8749549b
saulprl <phasecross@hotmail.com>
2024-06-14 12:15:19 -0700

    Add todos fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,45 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useEffect, useState } from "react";
 
-const DUMMY_TODOS = [
-  {
-    id: 1,
-    title: "Attend the React Course",
-  },
-  {
-    id: 2,
-    title: "Build a ToDo List",
-  },
-  {
-    id: 3,
-    title: "???",
-  },
-];
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+  userId: number;
+}
 
 function App() {
-  const [todos, setTodos] = useState(DUMMY_TODOS);
+  const [todos, setTodos] = useState<Array<Todo>>([]);
   const [inputValue, setInputValue] = useState("");
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos"
+      );
+
+      if (!response.ok) {
+        console.error("Failed to fetch todos");
+      }
+
+      const data = (await response.json()) as Todo[];
+      const slicedTodos = data.slice(0, 20);
+
+      setTodos(slicedTodos);
+      console.log("fetched todos");
+    };
+
+    fetchTodos();
+  }, []);
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     setTodos((prevTodos) => {
-      const newTodo = {
+      const newTodo: Todo = {
         id: Math.floor(Math.random() * 1000) + todos.length,
         title: inputValue,
+        completed: false,
+        userId: 1,
       };
 
       return [...prevTodos, newTodo];
@@ -52,25 +66,30 @@ function App() {
           <h2 className="text-xl font-medium whitespace-nowrap">My ToDos</h2>
           <form
             onSubmit={handleSubmit}
-            className="flex w-full items-center gap-2 "
+            className="flex w-full items-stretch gap-2"
           >
             <input
               type="text"
               value={inputValue}
               onChange={handleInputChange}
-              className="flex-grow"
+              className="flex-grow px-2 bg-purple-300 rounded-md"
             />
-            <button>Submit</button>
+            <button className="p-2 rounded font-medium bg-[#7145d6]">
+              Submit
+            </button>
           </form>
         </div>
         <div className="p-2 bg-violet-300 flex-grow h-full rounded-md">
-          <ul className="list-inside list-disc">
+          <ul className="flex flex-col gap-2">
             {todos.map((todo) => {
               return (
-                <li key={todo.id}>
-                  <span>{todo.title}</span>
+                <li
+                  key={todo.id}
+                  className="bg-[#7145d6] p-2 rounded flex w-full gap-4 items-center"
+                >
+                  <span className="flex-grow">{todo.title}</span>
                   <button
-                    className="bg-[#e91e63]"
+                    className="bg-[#e91e63] p-2 rounded font-medium"
                     onClick={() => {
                       // logic here
                       handleDelete(todo.id);
